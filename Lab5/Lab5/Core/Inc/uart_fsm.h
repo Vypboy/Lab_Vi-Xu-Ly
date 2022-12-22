5a6689be864cd3e1a624c27133c40b2b5bfcf102
Vypboy <vytran210402@gmail.com>
2022-12-22 15:48:03 +0700

    add files Lab5

---
+++ Lab5/Lab5/Core/Inc/uart_fsm.h
@@ -0,0 +1,14 @@
+/*
+ * uart_fsm.h
+ *
+ *  Created on: Dec 12, 2022
+ *      Author: ADMIN
+ */
+
+#ifndef INC_UART_FSM_H_
+#define INC_UART_FSM_H_
+
+void command_parser_fsm();
+void uart_communiation_fsm();
+
+#endif /* INC_UART_FSM_H_ */
